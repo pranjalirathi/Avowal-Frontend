ab737773ea7babe60bd13eaa42d74b848c2ac7a8
Pranjali Rathi <pranjalirathi05@gmail.com>
2025-03-26 01:26:07 +0530

    adding dummy data for cards ui testing

---
+++ src/constants/confessionsData.jsx
@@ -0,0 +1,76 @@
+// confessionsData.js
+export const confessionsData = [
+  {
+    id: "1",
+    text: "I’ve been secretly learning to play the guitar for 6 months to surprise my partner on our anniversary. I’m so nervous they might not like it, but I’ve put my heart into this.",
+    comments: 12,
+    time: "2 hours ago",
+  },
+  {
+    id: "2",
+    text: "I told everyone @prashu @ayati I was sick last weekend so I could stay home and binge-watch an entire series. Best decision ever, but I feel a little guilty about all the concerned messages I received.",
+    comments: 8,
+    time: "5 hours ago",
+  },
+  {
+    id: "3",
+    text: "I’ve been pretending to like my friend’s cooking for years. It’s actually terrible, but they’re so proud of it that I can’t bring myself to tell them the truth.",
+    comments: 12,
+    time: "10 hours ago",
+  },
+  {
+    id: "4",
+    text: "I’ve been pretending to like my friend’s cooking for years. It’s actually terrible, but they’re so proud of it that I can’t bring myself to tell them the truth.",
+    comments: 4,
+    time: "10 hours ago",
+  },
+  {
+    id: "5",
+    text: "I’ve been pretending to like my friend’s cooking for years. It’s actually terrible, but they’re so proud of it that I can’t bring myself to tell them the truth.",
+    comments: 4,
+    time: "10 hours ago",
+  },
+  {
+    id: "6",
+    text: "I’ve been pretending to like my friend’s cooking for years. It’s actually terrible, but they’re so proud of it that I can’t bring myself to tell them the truth.",
+    comments: 4,
+    time: "10 hours ago",
+  },
+  {
+    id: "7",
+    text: "I’ve been pretending to like my friend’s cooking for years. It’s actually terrible, but they’re so proud of it that I can’t bring myself to tell them the truth.",
+    comments: 4,
+    time: "10 hours ago",
+  },
+  {
+    id: "8",
+    text: "I’ve been pretending to like my friend’s cooking for years. It’s actually terrible, but they’re so proud of it that I can’t bring myself to tell them the truth.",
+    comments: 4,
+    time: "10 hours ago",
+  },
+  {
+    id: "9",
+    text: "I’ve been pretending to like my friend’s cooking for years. It’s actually terrible, but they’re so proud of it that I can’t bring myself to tell them the truth.",
+    comments: 4,
+    time: "10 hours ago",
+  },
+  {
+    id: "10",
+    text: "I’ve been pretending to like my friend’s cooking for years. It’s actually terrible, but they’re so proud of it that I can’t bring myself to tell them the truth.",
+    comments: 4,
+    time: "10 hours ago",
+  },
+  {
+    id: "11",
+    text: "I’ve been pretending to like my friend’s ",
+    comments: 4,
+    time: "10 hours ago",
+  },
+  {
+    id: "12",
+    text: "I’ve been pretending to like my friend’s cooking for years. It’s actually terrible, but they’re so proud of it that I can’t bring myself to tell them the truth. ’ve been pretending to like my friend’s cooking for years. It’s actually terrible, but they’re so proud of it that I can’t bring myself to tell them the truth. ’ve been pretending to like my friend’s cooking for years. It’s actually terrible, but they’re so proud of it that I can’t bring myself to tell them the truth. ’ve been pretending to like my friend’s cooking for years. It’s actually terrible, but they’re so proud of it that I can’t bring myself to tell them the truth.",
+    comments: 4,
+    time: "10 hours ago",
+  },
+
+];
