47ea3b6d60a1f8f38eacac6a868889556293739d
Pranjali Rathi <pranjalirathi05@gmail.com>
2025-03-27 02:33:49 +0530

    added notification screen ui

--- src/screens/NotificationScreen.jsx
+++ src/screens/NotificationScreen.jsx
@@ -1,14 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
 
-const NotificationScreen = () => {
+const ComingSoonScreen = () => {
   return (
-    <View>
-      <Text>This is the NotificationScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
 
-export default NotificationScreen
+      <Text style={styles.title}>Notifications </Text>
 
-const styles = StyleSheet.create({})
+      <View style={styles.content}>
+        <Text style={styles.comingSoonText}>COMING SOON</Text>
+        <Text style={styles.description}>
+          We're working hard to bring you something amazing.
+        </Text>
+        <View style={styles.underline} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const { width } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121212",
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 24,
+    color: "#E94560",
+    fontWeight: "bold",
+    marginVertical: 16,
+    marginTop: 12,
+    marginBottom: 10,
+    padding: 10,
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 70
+  },
+  comingSoonText: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#E94560",
+    textAlign: "center",
+  },
+  description: {
+    fontSize: 16,
+    color: "#FFFFFF",
+    textAlign: "center",
+    marginTop: 8,
+  },
+  underline: {
+    width: width * 0.1,
+    height: 2,
+    backgroundColor: "#E94560",
+    marginTop: 8,
+  },
+});
+
+export default ComingSoonScreen;
