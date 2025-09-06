7d90fa147d1beb166e34e6384689067170d384b6
Pranjali Rathi <pranjalirathi05@gmail.com>
2025-09-06 22:41:40 +0530

    Fixed the regex of username with roll no highlight

--- src/helpers/renderContentWithMentions.jsx
+++ src/helpers/renderContentWithMentions.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Text } from "react-native";
 
 export const renderContentWithMentions = (text, baseStyle, mentionStyle) => {
-  const parts = text.split(/(@\w+)/g);
+  const parts = text.split(/(@[\w.]+)/g);
 
   return parts.map((part, index) => {
-    if (part.match(/^@\w+$/)) {
+    if (part.match(/^@[\w.]+$/)) {
       return (
         <Text key={index} style={[baseStyle, mentionStyle]}>
           {part}
